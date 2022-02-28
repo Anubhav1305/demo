164181df6368dee7bda4da643b56f4c87d2862e3
Anubhav BANSAL <anubhav.bansal@amadeus.com>
2022-02-28 21:17:07 +0530

    commiting done successfully

---
+++ html-css/java_script_file.js
@@ -0,0 +1,9 @@
+let arraysize=prompt("Tell the natural number array size");
+
+function make_array(arraysize){
+    for(let i=1;i<=arraysize;i++){
+        console.log(i);
+    }
+}
+
+make_array(arraysize);
